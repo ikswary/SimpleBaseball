64b25675de01ec90d299c54fc845fa40802bbb1d
ikswary <ikswary@naver.com>
2019-12-04 10:47:11 +0900

    Team Info class 설계
    String 인스턴스 멤버변수 Name과 선수의 이름, 타율을 가지는
    TeamMember 이너클래스의 인스턴스 멤버객체 hitter를 1~9까지 가진다

---
+++ src/TeamInfo.java
@@ -0,0 +1,25 @@
+public class TeamInfo {
+    class TeamMember{
+        String Name;
+        double hitRatio;
+    }
+    String teamName;
+    TeamMember hitter1;
+    TeamMember hitter2;
+    TeamMember hitter3;
+    TeamMember hitter4;
+    TeamMember hitter5;
+    TeamMember hitter6;
+    TeamMember hitter7;
+    TeamMember hitter8;
+    TeamMember hitter9;
+
+
+
+
+
+
+
+
+
+}
