2cb46fd82889cfdbcf27d359221a5bfd37ee0b8c
ikswary <ikswary@naver.com>
2019-12-04 16:34:54 +0900

    step-2 요구사항1 구현
    TeamInfo 클래스에  멤버변수 TeamMember객체의 값들을 프린트하는 printTeamData()메소드 추가

--- src/TeamInfo.java
+++ src/TeamInfo.java
@@ -10,16 +10,23 @@ public class TeamInfo{
         }
     }
 
-    public void inputTeamName(String name){
+    public void inputTeamName(String name) {
         this.teamName = name;
     }
 
     public void inputTeamMember(int i, String name, double ratio){
-        /* 기능 구현 위해 예제출력과 다르게 작성됨
+        /*
+           기능 구현 위해 예제출력과 다르게 작성됨
            차후 수정 필요
         */
         this.hitter[i].name = name;
         this.hitter[i].hitRatio = ratio;
-        System.out.println("Input player Num " + i + "> " + hitter[i].name+ " " + " " + hitter[i].hitRatio );
+    }
+
+    public void printTeamData(){
+        System.out.println("Team " + this.teamName + "players data" );
+        for (int i = 0; i<9; i++){
+            System.out.println("Num" + i + " " + this.hitter[i].name + ", " + this.hitter[i].hitRatio);
+        }
     }
 }
