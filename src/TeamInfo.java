c098fed144c97808e0dc7abf937eb3befdc392e7
ikswary <ikswary@naver.com>
2019-12-04 11:47:08 +0900

    Scanner 객체 innt 위치 변경
    Scanner 객체의 수명 단축을 위해 위치 변경 및 sc.close() 메소드 추가

--- src/TeamInfo.java
+++ src/TeamInfo.java
@@ -7,24 +7,26 @@ public class TeamInfo{
     }
     String teamName;
     TeamMember hitter[] = new TeamMember[9];
-    Scanner sc = new Scanner(System.in);
 
     public void inputTeamName(){
-        this.teamName = this.sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        this.teamName = sc.nextLine();
+        sc.close();
     }
 
     public void inputTeamMember(){
         /* 기능 구현 위해 예제출력과 다르게 작성됨
            차후 수정 필요
          */
+        Scanner sc = new Scanner(System.in);
+
         for(int i=0; i<9; i++){
             System.out.println("Input player Num" + i + "> name");
-            this.hitter[i].name = this.sc.next();
+            this.hitter[i].name = sc.next();
             System.out.println("Input player Num" + i + "> hit ratio(0.1 < ratio < 0.5");
-            this.hitter[i].hitRatio = this.sc.nextDouble();
+            this.hitter[i].hitRatio = sc.nextDouble();
         }
-
-
+        sc.close();
     }
 
 
